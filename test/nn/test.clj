4a1e84c5146d0fc57f6463147ae1393d3cda5d63
Nick Ewing <nick@nickewing.net>
2012-01-07 12:37:51 -0800

    Add more forward propagation tests

--- test/nn/test.clj
+++ test/nn/test.clj
@@ -3,14 +3,37 @@
         clojure.test
         (incanter core)))
 
+(def negate-network
+  {:weights
+   [(matrix [[10 -20]])]})
+
+(deftest binary-negate
+  (is (= 0 (run-binary negate-network [1])))
+  (is (= 1 (run-binary negate-network [0]))))
+
+(def and-network
+  {:weights
+   [(matrix [[-30 20 20]])]})
+
+(deftest binary-and
+  (is (= 0 (run-binary and-network [0 0])))
+  (is (= 0 (run-binary and-network [1 0])))
+  (is (= 0 (run-binary and-network [0 1])))
+  (is (= 1 (run-binary and-network [1 1]))))
+
 (def xnor-network
   {:weights
    [(matrix [[-30  20  20]
              [ 10 -20 -20]])
     (matrix [[-10  20  20]])]})
 
-(deftest forward-propagation
+(deftest binary-xnor
   (is (= 1 (run-binary xnor-network [0 0])))
   (is (= 0 (run-binary xnor-network [1 0])))
   (is (= 0 (run-binary xnor-network [0 1])))
   (is (= 1 (run-binary xnor-network [1 1]))))
+
+(deftest forward-propagation
+  (binary-negate)
+  (binary-and)
+  (binary-xnor))
