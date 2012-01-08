aa1c2ca186fb0387fc8e48c62de5649b659f4e4d
Nick Ewing <nick@nickewing.net>
2012-01-08 14:38:54 -0800

    Backpropagation with gradient descent

--- src/nn/core.clj
+++ src/nn/core.clj
@@ -14,23 +14,47 @@
   [v]
   (bind-rows [1] v))
 
-(defn- propagate-layer
+(defn- mmult-matrix
+  [a b]
+  (let [result (mmult a b)]
+    (if (matrix? result)
+      result
+      (matrix [result]))))
+
+(defn- forward-propagate-layer
   "Calculate activations for layer l+1 given weight matrix between layer l
   and l+1 and layer l activations."
   [weight-matrix activations]
-  (sigmoid (mmult weight-matrix (bind-bias activations))))
+  (sigmoid (mmult-matrix weight-matrix activations)))
+
+(defn- forward-propagate
+  [weights input-activations]
+  (let [input-activations (bind-bias input-activations)]
+    ((fn [all-weights next-activations all-activations]
+       (let [weights (first all-weights)
+             all-weights (rest all-weights)
+             last-iter (empty? all-weights)
+             next-activations (forward-propagate-layer weights next-activations)
+             next-activations (if last-iter next-activations (bind-bias next-activations))
+             all-activations (conj all-activations next-activations)]
+         (if last-iter
+           all-activations
+           (recur all-weights next-activations all-activations))))
+     weights input-activations [input-activations])))
+
+(defn- output-from-activations
+  [activations]
+  (map identity (last activations)))
 
 (defn run
   "Run forward propagation using the given network and inputs."
   [network inputs]
   (let [weights (:weights network)
-        activations (matrix inputs)]
-    (reduce #(propagate-layer %2 %1) activations weights)))
+        input-activations (matrix inputs)]
+    (output-from-activations (forward-propagate weights input-activations))))
 
 (defn- round-output [output]
-  (if (matrix? output)
-    (map #(Math/round ^Double %) output)
-    (Math/round ^Double output)))
+  (vec (map #(Math/round ^Double %) output)))
 
 (defn run-binary
   "Run forward propagation on given network and inputs and then round
@@ -38,5 +62,120 @@
   [network inputs]
   (round-output (run network inputs)))
 
-(defn -main [& args]
-  (println (run-binary [0 0])))
+(def init-epsilon 0.2) ; TODO: choose a smarter value
+
+(defn- random-list
+  "Create a list of random doubles between -init-epsilon and +init-epsilon."
+  [len]
+  (for [x (range 0 len)]
+    (- (rand (* 2 init-epsilon)) init-epsilon)))
+
+(defn- random-weight-matrices
+  "Generate random initial weight matrices for given layer-sizes."
+  [layer-sizes]
+  (for [i (range 0 (dec (length layer-sizes)))]
+    (let [cols (inc (get layer-sizes i))
+          rows (get layer-sizes (inc i))]
+      (matrix (random-list (* rows cols)) cols))))
+
+(defn- back-propagate-layer-deltas
+  "Back propagate last-deltas (from layer l-1) and return layer l deltas."
+  [last-deltas weight-matrix layer-activations]
+  (mult (mmult-matrix (trans weight-matrix) last-deltas)
+        (mult layer-activations (minus 1 layer-activations))))
+
+(defn- calc-hidden-deltas
+  "Calculate hidden deltas for back propagation.  Returns all deltas including
+  output-deltas."
+  [weights activations output-deltas]
+  (let [hidden-weights (reverse (rest weights))
+        hidden-activations (rest (reverse (rest activations)))]
+    ((fn [last-deltas all-weights all-activations all-deltas]
+       (if (empty? all-weights)
+         all-deltas
+         (let [weights (first all-weights)
+               all-weights (rest all-weights)
+               activations (first all-activations)
+               all-activations (rest all-activations)
+               last-deltas (back-propagate-layer-deltas last-deltas weights activations)
+               all-deltas (cons (rest last-deltas) all-deltas)]
+           (recur last-deltas all-weights all-activations all-deltas))))
+     output-deltas hidden-weights hidden-activations (list output-deltas))))
+
+(defn- add-delta-sum
+  ""
+  [delta-sums deltas activations]
+  (map #(plus %1 (mmult %2 (trans %3)))
+       delta-sums deltas activations))
+
+(defn- init-delta-sums
+  [weights]
+  (map (fn [weight]
+         (let [[rows cols] (dim weight)]
+           (matrix 0 rows cols)))
+       weights))
+
+(defn- back-propagate-example
+  "Run one step of back propagation."
+  [delta-sums weights input expected-output]
+  (let [activations (forward-propagate weights (matrix input))
+        output (last activations)
+        output-deltas (minus output expected-output)
+        all-deltas (calc-hidden-deltas weights activations output-deltas)
+        delta-sums (add-delta-sum delta-sums all-deltas activations)]
+    delta-sums))
+
+(def max-epochs 20000)
+(def desired-error 1e-2)
+(def learning-rate 0.5)
+(def epochs-per-report 100)
+
+; TODO: handle regularization
+(defn- back-propagate-all-examples
+  ""
+  [weights examples]
+  (let [num-examples (length examples)
+        delta-sums (reduce
+                     #(back-propagate-example %1 weights (first %2) (second %2))
+                     (init-delta-sums weights) examples)
+        gradients (map #(div % num-examples) delta-sums)]
+    gradients))
+
+(defn- calc-mse
+  "Calculate mean squared error of predictions"
+  [weights examples]
+  (let [sum (reduce (fn [total example]
+                      (let [[input expected-output] example
+                            output (output-from-activations
+                                               (forward-propagate weights (matrix input)))]
+                        (+ total (sum (pow (minus output expected-output) 2)))))
+                    0 examples)]
+    (/ sum (length examples))))
+
+(defn- gradient-descent
+  ([weights examples i]
+   (let [gradients (back-propagate-all-examples weights examples)
+         new-weights (map (fn [weights gradients]
+                            (minus weights (mult learning-rate gradients)))
+                          weights gradients)
+         mse (calc-mse new-weights examples)]
+     (if (= (mod i epochs-per-report) 0)
+       (println "Epoch " i "MSE" mse))
+     (if (or (> i max-epochs) (< mse desired-error))
+       (do
+         (println "Total epochs:" i "Final MSE:" mse)
+         new-weights)
+       (recur new-weights examples (inc i)))))
+  ([weights examples]
+   (gradient-descent weights examples 0)))
+
+(defn train
+  "Train network on training examples."
+  [inputs outputs hidden-neurons]
+  (let [num-inputs (length (first inputs))
+        num-outputs (length (first outputs))
+        layer-sizes (conj (vec (cons num-inputs hidden-neurons)) num-outputs)
+        weights (random-weight-matrices layer-sizes)
+        examples (map #(vector %1 %2) inputs outputs)]
+    {:weights (gradient-descent weights examples)}))
+
--- test/nn/test.clj
+++ test/nn/test.clj
@@ -7,19 +7,19 @@
   {:weights
    [(matrix [[10 -20]])]})
 
-(deftest binary-negate
-  (is (= 0 (run-binary negate-network [1])))
-  (is (= 1 (run-binary negate-network [0]))))
+(deftest binary-negate-run
+  (is (= [0] (run-binary negate-network [1])))
+  (is (= [1] (run-binary negate-network [0]))))
 
 (def and-network
   {:weights
    [(matrix [[-30 20 20]])]})
 
-(deftest binary-and
-  (is (= 0 (run-binary and-network [0 0])))
-  (is (= 0 (run-binary and-network [1 0])))
-  (is (= 0 (run-binary and-network [0 1])))
-  (is (= 1 (run-binary and-network [1 1]))))
+(deftest binary-and-run
+  (is (= [0] (run-binary and-network [0 0])))
+  (is (= [0] (run-binary and-network [1 0])))
+  (is (= [0] (run-binary and-network [0 1])))
+  (is (= [1] (run-binary and-network [1 1]))))
 
 (def xnor-network
   {:weights
@@ -27,13 +27,26 @@
              [ 10 -20 -20]])
     (matrix [[-10  20  20]])]})
 
-(deftest binary-xnor
-  (is (= 1 (run-binary xnor-network [0 0])))
-  (is (= 0 (run-binary xnor-network [1 0])))
-  (is (= 0 (run-binary xnor-network [0 1])))
-  (is (= 1 (run-binary xnor-network [1 1]))))
+(deftest binary-xnor-run
+  (is (= [1] (run-binary xnor-network [0 0])))
+  (is (= [0] (run-binary xnor-network [1 0])))
+  (is (= [0] (run-binary xnor-network [0 1])))
+  (is (= [1] (run-binary xnor-network [1 1]))))
 
-(deftest forward-propagation
-  (binary-negate)
-  (binary-and)
-  (binary-xnor))
+(deftest binary-or-train
+  (let [inputs [[0 0] [0 1] [1 0] [1 1]]
+        outputs [[0] [1] [1] [1]]
+        network (train inputs outputs [])]
+    (is (= [0] (run-binary network [0 0])))
+    (is (= [1] (run-binary network [1 0])))
+    (is (= [1] (run-binary network [0 1])))
+    (is (= [1] (run-binary network [1 1])))))
+
+(deftest binary-xnor-train
+  (let [inputs [[0 0] [0 1] [1 0] [1 1]]
+        outputs [[1] [0] [0] [1]]
+        network (train inputs outputs [2])]
+    (is (= [1] (run-binary network [0 0])))
+    (is (= [0] (run-binary network [1 0])))
+    (is (= [0] (run-binary network [0 1])))
+    (is (= [1] (run-binary network [1 1])))))
