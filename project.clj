8103cb947f7fea989f12aec5debed7823950ac06
Nick Ewing <nick@nickewing.net>
2012-08-05 21:56:38 -0700

    Lock incanter version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject netz "0.1.0"
+(defproject netz "0.1.1"
   :description "Neural Network"
-  :dependencies [[incanter/incanter-core "1.3.0-SNAPSHOT"]
-                 [incanter/incanter-io "1.3.0-SNAPSHOT"]]
+  :dependencies [[incanter/incanter-core "1.3.0"]
+                 [incanter/incanter-io "1.3.0"]]
   :main netz.core)
